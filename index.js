4d3b2bf4ed997656a52c7eec77cb06b1cecedec1
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-12 20:39:45 +0700

    Add get all persons

--- index.js
+++ index.js
@@ -3,8 +3,37 @@ const app = express();
 
 const PORT = 3002;
 
+const persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
 app.get("/hello.html", (req, res) => {
   res.send("<h1>Hello!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
 });
 
 app.listen(PORT, () => {
