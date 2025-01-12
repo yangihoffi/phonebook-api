6f64748775164b2fe705a0d7bdf6f02f8108ce2e
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-12 21:07:28 +0700

    Implement all CRUD end points

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = 3002;
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -34,6 +35,62 @@ app.get("/hello.html", (req, res) => {
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+
+  const foundPerson = persons.find((person) => person.id === id);
+
+  if (foundPerson) {
+    res.json(foundPerson);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  const newPerson = {
+    id: uuidv4(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+
+  res.json(persons);
+});
+
+app.put("/api/persons/:id", (req, res) => {
+  const body = req.body;
+  const id = res.params.id;
+
+  const foundPerson = persons.find((person) => person.id === id);
+
+  if (foundPerson) {
+    const changedPerson = {
+      ...foundPerson,
+      name: body.name,
+      number: body.number,
+    };
+
+    persons.map((person) => (person.id === id ? changedPerson : person));
+
+    res.json(changedPerson);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+
+  const foundPerson = persons.find((person) => person.id === id);
+
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).json(foundPerson);
 });
 
 app.get("/info", (req, res) => {
