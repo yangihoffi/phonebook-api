e4b50de0dc790a171c5a9bd8a7d5919f06663f38
Huu Vuong Phan <166980411+yangihoffi@users.noreply.github.com>
2025-01-14 19:11:03 +0700

    Implement full CRUD

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
 const connectToDatabase = require("./database/db");
+const Person = require("./models/person.model");
 
 const app = express();
 
 const PORT = process.env.PORT;
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 connectToDatabase();
 
@@ -39,67 +16,68 @@ app.get("/hello.html", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
 
-  const foundPerson = persons.find((person) => person.id === id);
-
-  if (foundPerson) {
-    res.json(foundPerson);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(id).then((person) => {
+    if (person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  });
 });
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-  const newPerson = {
-    id: uuidv4(),
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "name or number missing" });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
+});
+
+app.put("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  const person = {
     name: body.name,
     number: body.number,
   };
 
-  persons = persons.concat(newPerson);
-
-  res.json(persons);
-});
-
-app.put("/api/persons/:id", (req, res) => {
-  const body = req.body;
-  const id = res.params.id;
-
-  const foundPerson = persons.find((person) => person.id === id);
-
-  if (foundPerson) {
-    const changedPerson = {
-      ...foundPerson,
-      name: body.name,
-      number: body.number,
-    };
-
-    persons.map((person) => (person.id === id ? changedPerson : person));
-
-    res.json(changedPerson);
-  } else {
-    res.status(404).end();
-  }
+  Person.findByIdAndUpdate(id, person, { new: true }).then((returnedPerson) => {
+    res.json(returnedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+
+  Person.findOneAndDelete(id).then(() => {
+    res.status(204).end();
+  });
 });
 
 app.get("/info", (req, res) => {
   const numOfPersons = persons.length;
   const date = new Date();
-  res.send(`<p>Phonebook has info for ${numOfPersons} people</p>
-    <p>${date.getDate()}</p>
-    `);
+  res.send(
+    `<p>Phonebook has info for ${numOfPersons} people</p><p>${date.getDate()}</p>`
+  );
 });
 
 app.listen(PORT, () => {
